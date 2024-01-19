891cf9dae3967fd7f40236f7c90994d2fc6d59e5
AbrahamRodri <rodriguez.abraham63@outlook.com>
2024-01-19 13:28:03 -0800

    Tweaks / Services 50% complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Hero from "./Components/Hero";
 import Nav from "./Components/Nav";
+import Services from "./Components/Services";
 
 const App = () => {
     return(
         <div className="bg-slate-900">
             <Nav/>
+            <Hero/>
+            <Services/>
         </div>
     );
 
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -9,7 +9,7 @@ const Nav = () => {
         setClick(!click)
     }
     const content = <> 
-        <div className="lg:hidden block absolute top-15 w-full left-0 right-0 bg-slate-900 transition">
+        <div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-slate-900 transition">
             <ul className="text-center text-xl p-20">
                 <Link spy={true} smooth ={true} to="Services">
                     <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Services</li>
@@ -29,7 +29,7 @@ const Nav = () => {
                 <div className ="flex items-center flex-1">
                     <span className="text-3xl font-bold">Logo</span>
                 </div>
-                <div className="lg:flex md:flex lg:flex-1 items center justify-end font-normal hidden">
+                <div className="lg:flex md:flex lg: flex-1 items center justify-end font-normal hidden">
                     <div className="flex-10">
                         <ul className="flex gap-8 mr-16 text-[18px]">
                             <Link spy={true} smooth ={true} to="Services">
@@ -46,11 +46,11 @@ const Nav = () => {
                 </div>
                 <div>
                         {click && content}
-                    </div>
+                </div>
 
-                    <button className="block md:hidden transiton" onClick={handleClick}>
+                <button className="block md:hidden transiton" onClick={handleClick}>
                         {click ? <FaTimes/> : <CiMenuFries/>}
-                    </button>
+                </button>
             </div>
         </nav>
     );
