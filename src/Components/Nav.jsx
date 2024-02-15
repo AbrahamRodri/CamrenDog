c06394ad22ce276700979ac4150ce0b04dceb3d7
Abraham Rodriguez <rodriguez.abraham63@outlook.com>
2024-02-14 18:22:02 -0800

    up

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -28,7 +28,7 @@ const Nav = () => {
             <div className ="h-10vh flex justify-between z-50 text-white lg:py-5 px-20 py-4 flex-1">
                 <div className ="flex items-center flex-1">
                     <span className="text-3xl font-bold flex font-round">
-                        <img className="h-10 mr-2"src="./logo.png" alt="" />
+                        <img className="h-10 mr-2"src="logo.png" alt="" />
                         K9 PROMO
                         </span>
                 </div>
